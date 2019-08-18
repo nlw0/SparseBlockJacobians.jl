2f0ae064036f119c72c903c230a064150647e6df
Nicolau Leal Werneck <nwerneck@gmail.com>
2019-08-18 18:30:58 +0200

    drafting the sparse Jacobian updates, still need to test

--- src/SparseBlockJacobians.jl
+++ src/SparseBlockJacobians.jl
@@ -1,15 +1,14 @@
 module SparseBlockJacobians
 
 export
-    update_residuals!, update_jacobian!
-
+    update_residuals!, update_jacobian_values!, update_jacobian_indices!
 
 @inline foldlargs(op, x) = x
 @inline foldlargs(op, x1, x2, xs...) = foldlargs(op, op(x1, x2), xs...)
 
 function update_residuals!(out::Array{Float64}, funcs)
     foldlargs(0, funcs...) do offset, (range, fun)
-        @inbounds for i in 1:range
+        for i in 1:range
             out[i + offset] = fun(i)
         end
         offset + range
@@ -17,10 +16,24 @@ function update_residuals!(out::Array{Float64}, funcs)
     nothing
 end
 
-function update_jacobian!(out::Array{Float64}, funcs)
+function update_jacobian_values!(out, funcs)
     foldlargs(0, funcs...) do offset, (range, block_size, fun)
-        @inbounds for i in 1:block_size:(1+block_size*range)
-            out[i + offset:i + offset+block_size] .= fun(i)
+        for i in 1:range
+            out.nzval[1+block_size*i + offset:1+block_size*i + offset+block_size] .= fun(i)
+        end
+        offset + range
+    end
+    nothing
+end
+
+function update_jacobian_indices!(out, funcs)
+    colacc = 1
+    foldlargs(0, funcs...) do offset, (range, block_size, fun)
+        for i in 0:(range-1)
+            display("$i, $(fun(i))")
+            out.rowval[1+block_size*i + offset:1+block_size*i + offset+block_size] .= fun(i)
+            out.colptr[1+i + offset:1+i + offset+block_size] .= colacc
+            colacc += block_size
         end
         offset + range
     end
